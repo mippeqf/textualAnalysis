0c7fe6bdcebe82f5f6dfb9a69748c2e235ec9877
Markus <broby@web.de>
2021-05-07 21:35:22 +0200

    fix: term drought around 2000 - missing close tags

--- 1 textProcessing.py
+++ 1 textProcessing.py
@@ -1,48 +1,52 @@
-# PURPOSE
-# Apply LDA and NMF
-# Use the paper where Hillert implemented it as a basis
-
-# Use this article for both LDA and NNMF
-# https://stackabuse.com/python-for-nlp-topic-modeling/#:~:text=Topic%20modeling%20is%20an%20unsupervised,clusters%20based%20on%20similar%20characteristics.
-
-# For LDA, use parameter "number of topics" N = 1 to test for whole-document relevance
-
 import os
-from gensim.models import Phrases
 import pickle
-from bs4 import BeautifulSoup
-from tqdm import tqdm
-import spacy
+import csv
+import re
+
 import requests
+import spacy
+from bs4 import BeautifulSoup
+from gensim.models import Phrases
+from tqdm import tqdm
 from wordcloud import WordCloud
+
 nlp = spacy.load("en_core_web_lg")
 nlp.max_length = 1500000  # Set up buffer length
 
 minutes = pickle.load(open(os.path.join(os.path.dirname(__file__), "data", "1fomcLinks"), "rb"))
 # minutes.reverse()  # DEBUG starting from 1993
 
-# Container for filtered paragraphs (ie sets of filtered tokens)
-# Needed for model training and word clouds. Structurally not necessary, can be derived from the DL array at any time
-# filteredParagraphs = []
 minutesNew = []
-# rawParagraphs = []  # Tokenized paragraphs without lemmatizing and type filtering - robustness test
 
 for row in tqdm(minutes):
-
     soup = BeautifulSoup(requests.get(row["link"]).content, "html.parser")
+    body = str(soup)
     text = soup.find("div", id="content") if soup.find("div", id="content") != None else soup.find("body")
 
     # Break document into paragraphs with min length 100 characters
     # text.split("\n") doesn't work, minutes before 2000 use \n for every linebreak even within paragraphs!
-    paragraphsTemp = text.find_all("p")
+    # Straightforward solution: Get textContent of <p> tags
+    # Major problem: Using Html closing tags apparently ran counter to Greenspan era policy, so using the above rule
+    # yields no text in many instances from around 1997 to 2003.
+    # Solution: check whether there are any closing tags at all and apply different splitting method if so.
+    paragraphstmp = []
     paragraphs = []
-    for para in paragraphsTemp:
-        if not len(para.find_all("p")) and len(para.get_text()) > 300:  # If no other p tags within the paragraph and min length
-            paragraphs.append(para.get_text())
+    if not body.count("</p>"):
+        paragraphstmp = body.split("<p>")  # Split the document on opening p tags
+        paragraphstmp.pop(0)  # Remove the first section before the first p tag
+        for para in paragraphstmp:
+            if not len(paragraphstmp) and len(para) > 300:
+                paragraphs.append(re.sub('<[^>]*>', '', para))
+    else:
+        paragraphstmp = text.find_all("p")
+        for para in paragraphstmp:
+            # assert "<p>" not in str(para) and "</p>" not in para, ("Found a p tag in the results", para, row["link"])
+            if len(para.text) > 300:
+                paragraphs.append(para.text)
 
     DLfilteredparagraphs = []
     DLrawparagraphs = []
-    for para in paragraphs:
+    for para in tqdm(paragraphs, leave=False):
         # Run standard spacy classification pipeline (tokenization, identification of word type etc)
         paraClassified = nlp(para)
         filteredTokens = []
@@ -51,7 +55,6 @@ for row in tqdm(minutes):
             if token.is_stop == False and token.is_punct == False and (token.pos_ == "NOUN" or token.pos_ == "ADJ" or token.pos_ == "VERB"):
                 filteredTokens.append(token.lemma_.lower())
             rawTokens.append(token.lower_)
-        # filteredParagraphs.append(filteredTokens)
         DLfilteredparagraphs.append(filteredTokens)
         DLrawparagraphs.append(rawTokens)  # only tokenized and decapitalised, no type filtering
     minutesNew.append({**row, "filteredParagraphs": DLfilteredparagraphs, "rawParagraphs": DLrawparagraphs})
@@ -73,3 +76,8 @@ pickle.dump(minutesNew, open(os.path.join(os.path.dirname(__file__), "data", "2D
 # Done for now
 # TODO optional: Fine tune bigram parameter
 # TODO optional: admin section cutoff by occurence of word "vote"
+
+with open(os.path.join(os.path.dirname(__file__), "data", "processedText.csv"), "w", newline="", encoding="utf-8") as f:
+    writer = csv.DictWriter(f, minutesNew[0].keys())
+    writer.writeheader()
+    writer.writerows(minutesNew)
