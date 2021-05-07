7fa22cd43848d5cfa6f54f7b34ae59dc8620aeb9
Markus <broby@web.de>
2021-05-07 21:36:59 +0200

    feat: implement top topic proportion variable

--- 2.3 toneComputation.py
+++ 2.3 toneComputation.py
@@ -36,9 +36,7 @@ lmPOS = set(lmRAW.query('Positive > 0')['Word'])
 lmNEG = set(lmRAW.query('Negative > 0')['Word'])
 lmUNCERT = set(lmRAW.query('Uncertainty > 0')['Word'])
 
-####################################################
-# Document-level tone computation
-####################################################
+# Compute document-level tone/uncert and topic-specific scores
 minutes.reverse()
 for i, row in enumerate(minutes):
 
@@ -48,6 +46,8 @@ for i, row in enumerate(minutes):
     uncertScoreAggNmf = {i: 0 for i in range(0, 8)}
     docLevelNetToneScore = 0
     docLevelUncertScore = 0
+    posnegcounter = 0
+    uncertcounter = 0
     for paragraph in row["filteredParagraphs"]:
         if not len(paragraph):  # Skip if paragraph is empty, would crash the /len(paragraph) part without
             continue
@@ -86,19 +86,62 @@ for i, row in enumerate(minutes):
                        "DL_uncert": docLevelUncertScore, "posnegcnt": posnegcounter, "uncertcnt": uncertcounter})
     # print(i, "of", len(minutes), row["year"])
 
+print("done1")
+
+# Add topic proportions
 minutesNewNew = []
-for doc in tqdm(minutesNew):
+for doc in minutesNew:
     topicAgg = {i: 0 for i in range(0, 8)}
     totalLength = sum([len(para) for para in doc["rawParagraphs"]])  # total length of entire document
     for i, para in enumerate(doc["filteredParagraphs"]):
         paraLength = len(doc["rawParagraphs"][i])
+        # print(lda.get_document_topics(dct.doc2bow(para)))
         for index, weight in lda.get_document_topics(dct.doc2bow(para)):
             topicAgg[index] += weight*paraLength/totalLength
     topicProps = {"propTopic"+str(key+1): value for key, value in topicAgg.items()}
     minutesNewNew.append({**doc, **topicProps})
 
+print("done2")
+
+# Add top topic proportions
+# Yes, it would be a lot more efficient if combined with the first for-loop, but I wouldn't understand anything two weeks down the road
+minutesNewNewNew = []
+for doc in minutesNewNew:
+    topTopicLdaAgg = {i: 0 for i in range(0, 8)}
+    topTopicNmfAgg = {i: 0 for i in range(0, 8)}
+    for i, para in enumerate(doc["filteredParagraphs"]):
+        if not len(para):  # Skip if paragraph is empty, would crash the /len(paragraph) part without
+            continue
+        bow = dct.doc2bow(para)  # generate word vector / bag-of-words from tokenized paragraph
+        ldatopics = lda.get_document_topics(bow, minimum_phi_value=0.01)  # Paramter necessary, bug in the library
+        nmftopics = nmf.get_document_topics(bow)
+        topTopicLdaAgg[sorted(ldatopics, key=lambda tup: tup[1], reverse=True)[0][0]] += 1
+        if len(nmftopics):
+            topTopicNmfAgg[sorted(nmftopics, key=lambda tup: tup[1], reverse=True)[0][0]] += 1
+    # Divide topTopic counts by the total number of paragraphs, blows up list comprehension for some reason
+    for key, value in topTopicLdaAgg.items():
+        if not sum(0 if val == "." else val for val in topTopicLdaAgg.values()):
+            topTopicLdaAgg[key] = "."  # missing
+        else:
+            topTopicLdaAgg[key] = value/sum(0 if val == "." else val for val in topTopicLdaAgg.values())
+    for key, value in topTopicNmfAgg.items():
+        if not sum(0 if val == "." else val for val in topTopicNmfAgg.values()):
+            topTopicNmfAgg[key] = "."  # missing
+        else:
+            topTopicNmfAgg[key] = value/sum(0 if val == "." else val for val in topTopicNmfAgg.values())
+    topTopicPropLda = {"topTopicPropLda"+str(key+1): value for key, value in topTopicLdaAgg.items()}
+    topTopicPropNmf = {"topTopicPropNmf"+str(key+1): value for key, value in topTopicNmfAgg.items()}
+    minutesNewNewNew.append({**doc, **topTopicPropLda, **topTopicPropNmf})
+
+print("done3")
+
+# Reduce size of dataset before exporting
+for i, mins in enumerate(minutesNewNewNew):
+    del mins["rawParagraphs"]
+    del mins["filteredParagraphs"]
+    minutesNewNewNew[i] = mins
 # Dump dataset containing timeseries of textual analysis to csv for Stata
 with open(os.path.join(os.path.dirname(__file__), "data", "dataExport.csv"), "w", newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(f, minutesNewNew[0].keys())
+    writer = csv.DictWriter(f, minutesNewNewNew[0].keys())
     writer.writeheader()
-    writer.writerows(minutesNewNew)
+    writer.writerows(minutesNewNewNew)
