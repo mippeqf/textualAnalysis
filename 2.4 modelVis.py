5095f3b819603e73c2727cf1c4d84032f24c3eac
Markus <broby@web.de>
2021-05-08 19:49:15 +0200

    implement wordclouds

--- 2.4 modelVis.py
+++ 2.4 modelVis.py
@@ -6,11 +6,8 @@ from gensim.models import LdaModel
 from gensim.models.nmf import Nmf
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
-import pickle
 import gensim.corpora
 import gensim.utils
-from tqdm import tqdm
-import pandas as pd
 
 #  TODO
 # - Proportion evolution (see Medium article)
@@ -25,7 +22,7 @@ import pandas as pd
 # LDAvis
 #################################################################
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 lda = LdaModel.load("models/lda")
 dct = gensim.utils.SaveLoad.load("models/dct")
@@ -44,7 +41,24 @@ if False:
 
 # lda.print_topics(5, 10)
 
-minspickeled = pickle.load(open(os.path.join(os.path.dirname(__file__), "data", "2DLparagraphs"), "rb"))
-minutes = [para for doc in minspickeled for para in doc["filteredParagraphs"]]
+# minspickeled = pickle.load(open(os.path.join(os.path.dirname(__file__), "data", "2DLparagraphs"), "rb"))
+# minutes = [para for doc in minspickeled for para in doc["filteredParagraphs"]]
 
-# TODO WORDCLOUDS
+if not os.path.exists("img"):
+    os.mkdir("img")
+
+for topic in range(0, 8):
+    termslda = lda.show_topic(topic, topn=50)  # get_topic_terms would return words as dict IDs, not strings
+    # Model returns list of tuples, wordcloud wants a dictionary instead
+    wordcloudlda = WordCloud(background_color="white").generate_from_frequencies(dict(termslda))
+    plt.imshow(wordcloudlda)
+    plt.axis("off")
+    plt.savefig(f"img/lda_topic_{topic+1}.png")
+
+for topic in range(0, 8):
+    termsnmf = nmf.show_topic(topic, topn=50)
+    # Model returns list of tuples, wordcloud wants a dictionary instead
+    wordcloudnmf = WordCloud(background_color="white").generate_from_frequencies(dict(termsnmf))
+    plt.imshow(wordcloudnmf)
+    plt.axis("off")
+    plt.savefig(f"img/nmf_topic_{topic+1}.png")
